bcce814efbc1995339b242414836d093e11b267a
rgopavarapu <rxg02980@ucmo.edu>
2020-02-13 14:31:11 -0800

    Initial commit

--- src/test/java/stepDefinition/HerokuLogin.java
+++ src/test/java/stepDefinition/HerokuLogin.java
@@ -1,24 +1,23 @@
 package stepDefinition;
 
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class HerokuLogin {
     WebDriver driver;
+    String Expected = "Welcome to the Secure Area. When you are done click logout below.";
 
     @Given("User opens Application$")
     public void user_opens_application() throws Throwable {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("http://http://the-internet.herokuapp.com/login");
+        driver.get("http://the-internet.herokuapp.com/login");
         System.out.println("Executed Given");
 
 //        throw new PendingException();
@@ -26,12 +25,24 @@ public class HerokuLogin {
 
     @When("User provide username and Password$")
     public void user_provide_username_and_password() throws Throwable {
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//*[@id=\"login\"]/button/i")).click();
+
         System.out.println("Executed When");
 //        throw new PendingException();
     }
 
     @Then("User is able to login$")
     public void user_is_able_to_login() throws Throwable {
+        String text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h4")).getText();
+        if (text.equalsIgnoreCase(Expected)) {
+            System.out.println("Test Passed");
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/a/i")).click();
+        } else {
+            System.out.println("Test failed");
+        }
+
         driver.quit();
         System.out.println("Executed Then");
 //        throw new PendingException();
